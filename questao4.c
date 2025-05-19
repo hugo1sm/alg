5c2c92d99df04819f599335f0f31ef6f0a167d96
hugo1sm <hugomagalhaesbds@gmail.com>
2025-05-19 11:06:49 -0300

    Add files via upload

---
+++ questao4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+      int num;
+      int i;
+      int soma = 0;
+      printf ("Digite um numero \n");
+      scanf ("%d", &num);
+      
+      for (i=0; i<=num; i++)
+      {
+          soma = soma + i;
+      }
+      printf ("A soma e = %d \n", soma);
+      
+           
+}
